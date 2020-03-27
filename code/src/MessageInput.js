c8ddc2b641da117031f06d3e17bf3874ec537bd1
Olle Alm <Olle@Olles-MacBook-Pro-2.local>
2020-03-27 21:53:11 +0100

    update messge localy

--- code/src/MessageInput.js
+++ code/src/MessageInput.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import './messageInput.css'
 
 
-export const MessageInput = () => {
+export const MessageInput = ({ setMessages }) => {
 
   const url = "https://technigo-thoughts.herokuapp.com/";
-  const [message, setMessage] = useState("");
+  const [postedMessage, setPostedMessage] = useState([]);
 
   const handelSubmit = (event) => {
     event.preventDefault();
@@ -15,16 +15,16 @@ export const MessageInput = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ message })
-      }
-    ).then(() => {
-      window.location.reload();
-    });
+        body: JSON.stringify({ message: postedMessage })
+      })
+      // .then(() => {
+      //   window.location.reload();
+      // });
 
-    // ).then((res) => res.json())
-    //   .then((newMessage) => {
-    //     setMessage((previousThoughts) => [newMessage, ...previousThoughts])
-    //   });
+      .then((res) => res.json())
+      .then((newMessage) => {
+        setMessages((previousThoughts) => [newMessage, ...previousThoughts])
+      });
   };
 
 
@@ -35,13 +35,13 @@ export const MessageInput = () => {
           rows="3"
           // cols="50"
           className="form-text"
-          value={message}
-          onChange={event => setMessage(event.target.value)}
+          value={postedMessage}
+          onChange={event => setPostedMessage(event.target.value)}
         ></textarea>
-        <p className={message.length > 140 ? "to-long" : ""} > {Math.abs(140 - message.length)} characters {message.length < 140 ? "left" : "to long"} </p>
+        <p className={postedMessage.length > 140 ? "to-long" : ""} > {Math.abs(140 - postedMessage.length)} characters {postedMessage.length < 140 ? "left" : "too long"} </p>
         <input
           type="submit"
-          disabled={message.length < 5 || message.length > 140 ? true : false}
+          disabled={postedMessage.length < 5 || postedMessage.length > 140 ? true : false}
           className="form-button"
           value="Add message">
         </input>
