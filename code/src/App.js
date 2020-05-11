09275f2176b6f00972243b86d92d10a8d0973683
Olle Alm <olle.alm@me.com>
2020-05-11 18:01:20 +0200

    loading component

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { MessageList } from './MessageList'
-import { MessageInput } from './MessageInput'
+import { MessageList } from './components/MessageList'
+import { MessageInput } from './components/MessageInput'
+import { Loading } from './components/Loading'
 import './app.css'
 
 export const App = () => {
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true)
   const url = "https://technigo-thoughts.herokuapp.com/";
 
   useEffect(() => {
+    setLoading(true);
     fetch(url)
       .then((res) => {
         return res.json();
       })
       .then(data => {
-        setMessages(data);
+        setTimeout(() => { // timeout to always show loader
+          setMessages(data);
+          setLoading(false)
+        }, 1500)
       });
   }, []);
 
@@ -22,6 +28,8 @@ export const App = () => {
 
       <MessageInput
         setMessages={setMessages} />
+
+      <Loading loading={loading} />
 
       <MessageList
         messages={messages}
